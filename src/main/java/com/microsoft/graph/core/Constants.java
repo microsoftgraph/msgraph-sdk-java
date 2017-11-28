3539fd5f4121842aee1b740be95dbc22a8cb69d9
Caitlin Bales (MSFT) <caitbal@microsoft.com>
2017-11-28 14:04:07 -0800

    Add Constants file

---
+++ src/main/java/com/microsoft/graph/core/Constants.java
@@ -0,0 +1,15 @@
+package com.microsoft.graph.core;
+
+public final class Constants {
+	private Constants() {
+	}
+	
+	public static final String APPROOT = "approot";
+		
+	// Constants for functional tests
+	// TO-DO: document how to register an application for functional 
+	// testing purposes
+	public static final String APPID = "app-id";
+	public static final String USERNAME = "user@email.com";
+	public static final String PASSWORD = "password";
+}
