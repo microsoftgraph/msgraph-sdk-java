d87544cadc2bd81c5c26cd5009abf244738959f6
Julian Raufelder <Julian@Raufelder.com>
2019-06-03 10:17:13 +0200

    microsoftgraph/msgraph-sdk-java#211 use primitive long type for ChunkedUpload size

--- src/main/java/com/microsoft/graph/concurrency/ChunkedUploadProvider.java
+++ src/main/java/com/microsoft/graph/concurrency/ChunkedUploadProvider.java
@@ -82,7 +82,7 @@ public class ChunkedUploadProvider<UploadType> {
     /**
      * The stream size
      */
-    private final Long streamSize;
+    private final long streamSize;
 
     /**
      * The upload response handler
@@ -106,7 +106,7 @@ public class ChunkedUploadProvider<UploadType> {
     public ChunkedUploadProvider(final UploadSession uploadSession,
                                  final IGraphServiceClient client,
                                  final InputStream inputStream,
-                                 final Long streamSize,
+                                 final long streamSize,
                                  final Class<UploadType> uploadTypeClass) {
         if (uploadSession == null) {
             throw new InvalidParameterException("Upload session is null.");
