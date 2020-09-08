b88d766252b8115b7c7049a5900780ad4417d70d
Vincent Biret <vibiret@microsoft.com>
2020-09-08 11:46:49 -0400

    - migrates test provider to v2 endpoint

--- src/main/java/com/microsoft/graph/core/Constants.java
+++ src/main/java/com/microsoft/graph/core/Constants.java
@@ -28,5 +28,7 @@ public final class Constants {
 	public static final String APPID = "app-id";
 	public static final String USERNAME = "user@email.com";
 	public static final String PASSWORD = "password";
+	public static final String TENANTID = "tenantid";
+	public static final String CLIENTSECRET = "clientsecret";
 	public static final String VERSION_NAME = "2.0.0";
 }
