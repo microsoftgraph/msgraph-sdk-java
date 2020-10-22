d158f182db772cf09ff6327a5f699cfeae13fe25
Vincent Biret <vibiret@microsoft.com>
2020-10-22 09:13:39 -0400

    - fixes collection request tests

--- src/test/java/com/microsoft/graph/http/BaseCollectionRequestTests.java
+++ src/test/java/com/microsoft/graph/http/BaseCollectionRequestTests.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +28,7 @@ import okhttp3.ResponseBody;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.microsoft.graph.concurrency.MockExecutors;
+import com.microsoft.graph.core.IBaseClient;
 import com.microsoft.graph.core.MockBaseClient;
 import com.microsoft.graph.logger.MockLogger;
 import com.microsoft.graph.options.FunctionOption;
@@ -40,53 +42,86 @@ import com.microsoft.graph.serializer.MockSerializer;
 public class BaseCollectionRequestTests {
 
     private MockBaseClient mBaseClient;
-    private BaseCollectionRequest<JsonObject, MockJsonObjectCollectionResponse> mRequest;
+    @SuppressWarnings("unchecked")
+    private Class<ICollectionResponse<JsonObject>> jsonObjectCollectionResponseMockClass = (Class<ICollectionResponse<JsonObject>>)mock(ICollectionResponse.class).getClass();
+    @SuppressWarnings("unchecked")
+    private Class<BaseCollectionPage<JsonObject>> jsonObjectCollectionPageMockClass = (Class<BaseCollectionPage<JsonObject>>)mock(BaseCollectionPage.class).getClass();
+    @SuppressWarnings("unchecked")
+    private Class<BaseCollectionRequestBuilder<JsonObject, 
+                                        BaseRequestBuilder<JsonObject>, 
+                                        ICollectionResponse<JsonObject>, 
+                                        BaseCollectionPage<JsonObject>, 
+                                        BaseCollectionRequest<JsonObject, ICollectionResponse<JsonObject>, BaseCollectionPage<JsonObject>>>> jsonObjectCollectionRequestBuilderMockClass = 
+                                        (Class<BaseCollectionRequestBuilder<JsonObject, 
+                                        BaseRequestBuilder<JsonObject>, 
+                                        ICollectionResponse<JsonObject>, 
+                                        BaseCollectionPage<JsonObject>, 
+                                        BaseCollectionRequest<JsonObject, ICollectionResponse<JsonObject>, BaseCollectionPage<JsonObject>>>>)
+                                        mock(BaseCollectionRequestBuilder.class).getClass();
+    private BaseCollectionRequest<JsonObject, ICollectionResponse<JsonObject>, BaseCollectionPage<JsonObject>> mRequest;
 
     @Before
-    @SuppressWarnings("unchecked")
     public void setUp() throws Exception {
         mBaseClient = new MockBaseClient();
         final Response response = new Response.Builder()
                 .request(new Request.Builder().url("https://a.b.c").build())
                 .protocol(Protocol.HTTP_1_1)
                 .code(200).message("OK").body(
-                   ResponseBody.create(
+                    ResponseBody.create(
                         "[{ \"id\": \"zzz\" }]",
                         MediaType.parse("application/json")
                 ))
                 .addHeader("Content-Type", "application/json")
                 .build();
         final OkHttpClient mockClient = BaseStreamRequestTests.getMockClient(response);
-        final JsonObject result = new JsonObject();
-        result.add("id", new JsonPrimitive("zzz"));
-        CoreHttpProvider mProvider = new CoreHttpProvider(new MockSerializer(result, ""),
+        final JsonObject resultobj = new JsonObject();
+        resultobj.add("id", new JsonPrimitive("zzz"));
+        @SuppressWarnings("unchecked")
+        final ICollectionResponse<JsonObject> result = mock(ICollectionResponse.class);
+        when(result.values()).thenReturn(new ArrayList<JsonObject>(Arrays.asList(resultobj)));
+        CoreHttpProvider mProvider = new CoreHttpProvider(new MockSerializer(result, "[{ \"id\": \"zzz\" }]"),
                 new MockExecutors(),
                 new MockLogger(),
                 mockClient);
         mBaseClient.setHttpProvider(mProvider);
-        final BaseCollectionPage<JsonObject> jsonObjectPage = new BaseCollectionPage<JsonObject>(new MockJsonObjectCollectionResponse(), null);
-        mRequest = new BaseCollectionRequest<JsonObject,MockJsonObjectCollectionResponse>("https://a.b.c/", mBaseClient, null, MockJsonObjectCollectionResponse.class, (Class<BaseCollectionPage<JsonObject>>)jsonObjectPage.getClass()){};
+        mRequest = new BaseCollectionRequest<JsonObject,ICollectionResponse<JsonObject>, BaseCollectionPage<JsonObject>>("https://a.b.c/", mBaseClient, null, jsonObjectCollectionResponseMockClass, jsonObjectCollectionPageMockClass, jsonObjectCollectionRequestBuilderMockClass){};
     }
 
     @Test
     public void testSend() {
-        final MockJsonObjectCollectionResponse result = mRequest.send();
+        final ICollectionResponse<JsonObject> result = mRequest.send();
         assertNotNull(result);
         assertEquals("zzz", result.values().get(0).get("id").getAsString());
     }
 
     @Test
     public void testPost() {
-        final MockJsonObjectCollectionResponse result = mRequest.post(null);
+        final ICollectionResponse<JsonObject> result = mRequest.post(null);
         assertNotNull(result);
         assertEquals("zzz", result.values().get(0).get("id").getAsString());
     }
 
+    @SuppressWarnings("unchecked")
+    private Class<ICollectionResponse<String>> stringCollectionResponseMockClass = (Class<ICollectionResponse<String>>)mock(ICollectionResponse.class).getClass();
+    @SuppressWarnings("unchecked")
+    private Class<BaseCollectionPage<String>> stringCollectionPageMockClass = (Class<BaseCollectionPage<String>>)mock(BaseCollectionPage.class).getClass();
+    @SuppressWarnings("unchecked")
+    private Class<BaseCollectionRequestBuilder<String, 
+                                        BaseRequestBuilder<String>, 
+                                        ICollectionResponse<String>, 
+                                        BaseCollectionPage<String>, 
+                                        BaseCollectionRequest<String, ICollectionResponse<String>, BaseCollectionPage<String>>>> stringCollectionRequestBuilderMockClass = 
+                                        (Class<BaseCollectionRequestBuilder<String, 
+                                        BaseRequestBuilder<String>, 
+                                        ICollectionResponse<String>, 
+                                        BaseCollectionPage<String>, 
+                                        BaseCollectionRequest<String, ICollectionResponse<String>, BaseCollectionPage<String>>>>)
+                                        mock(BaseCollectionRequestBuilder.class).getClass();
     @Test
     public void testFunctionParameters() {
         final Option f1 = new FunctionOption("1", "one");
         final Option f2 = new FunctionOption("2", null);
-        final BaseCollectionRequest<String,ICollectionResponse<String>> request = new BaseCollectionRequest<String,ICollectionResponse<String>>("https://a.b.c/", null, Arrays.asList(f1, f2), null,null){};
+        final BaseCollectionRequest<String,ICollectionResponse<String>, BaseCollectionPage<String>> request = new BaseCollectionRequest<String,ICollectionResponse<String>, BaseCollectionPage<String>>("https://a.b.c/", mock(IBaseClient.class), Arrays.asList(f1, f2), stringCollectionResponseMockClass, stringCollectionPageMockClass, stringCollectionRequestBuilderMockClass){};
         assertEquals("https://a.b.c/(1='one',2=null)", request.getRequestUrl().toString());
         request.addFunctionOption(new FunctionOption("3","two"));;
         assertEquals("https://a.b.c/(1='one',2=null,3='two')", request.getRequestUrl().toString());
@@ -97,7 +132,7 @@ public class BaseCollectionRequestTests {
     public void testQueryParameters() {
         final Option q1 = new QueryOption("q1","option1 ");
         final Option q2 = new QueryOption("q2","option2");
-        final BaseCollectionRequest<String,ICollectionResponse<String>> request = new BaseCollectionRequest<String,ICollectionResponse<String>>("https://a.b.c/", null, Arrays.asList(q1, q2), null,null){};
+        final BaseCollectionRequest<String,ICollectionResponse<String>, BaseCollectionPage<String>> request = new BaseCollectionRequest<String,ICollectionResponse<String>, BaseCollectionPage<String>>("https://a.b.c/", mock(IBaseClient.class), Arrays.asList(q1, q2), stringCollectionResponseMockClass, stringCollectionPageMockClass, stringCollectionRequestBuilderMockClass){};
         assertEquals("https://a.b.c/?q1=option1%20&q2=option2", request.getRequestUrl().toString());
         request.addQueryOption(new QueryOption("q3","option3"));
         assertEquals("https://a.b.c/?q1=option1%20&q2=option2&q3=option3", request.getRequestUrl().toString());
@@ -110,7 +145,7 @@ public class BaseCollectionRequestTests {
         final Option f2 = new FunctionOption("f2", null);
         final Option q1 = new QueryOption("q1","option1 ");
         final Option q2 = new QueryOption("q2","option2");
-        final BaseCollectionRequest<String,ICollectionResponse<String>> request = new BaseCollectionRequest<String,ICollectionResponse<String>>("https://a.b.c/", null, Arrays.asList(f1, f2, q1, q2), null,null){};
+        final BaseCollectionRequest<String,ICollectionResponse<String>, BaseCollectionPage<String>> request = new BaseCollectionRequest<String,ICollectionResponse<String>, BaseCollectionPage<String>>("https://a.b.c/", mock(IBaseClient.class), Arrays.asList(f1, f2, q1, q2), stringCollectionResponseMockClass, stringCollectionPageMockClass, stringCollectionRequestBuilderMockClass){};
         assertEquals("https://a.b.c/(f1='fun1',f2=null)?q1=option1%20&q2=option2", request.getRequestUrl().toString());
         assertEquals(5, request.getOptions().size());
     }
@@ -132,7 +167,7 @@ public class BaseCollectionRequestTests {
     public void testHeader() {
         final String expectedHeader = "header key";
         final String expectedValue = "header value";
-        final BaseCollectionRequest<String,ICollectionResponse<String>> request = new BaseCollectionRequest<String,ICollectionResponse<String>>("https://a.b.c/", null, null, null,null){};
+        final BaseCollectionRequest<String,ICollectionResponse<String>, BaseCollectionPage<String>> request = new BaseCollectionRequest<String,ICollectionResponse<String>, BaseCollectionPage<String>>("https://a.b.c/", mock(IBaseClient.class), null, stringCollectionResponseMockClass, stringCollectionPageMockClass, stringCollectionRequestBuilderMockClass){};
         assertEquals(1, request.getHeaders().size());
         request.addHeader(expectedHeader,expectedValue);
         assertEquals(2,request.getHeaders().size());
