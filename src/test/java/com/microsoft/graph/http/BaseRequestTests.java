46d911699af0c2f4f6c3a0299de39c35d8f71d19
Vincent Biret <vibiret@microsoft.com>
2020-11-20 14:29:03 -0500

    - improves unit test for getNExtPage references return type

--- src/test/java/com/microsoft/graph/http/BaseRequestTests.java
+++ src/test/java/com/microsoft/graph/http/BaseRequestTests.java
@@ -50,9 +50,8 @@ public class BaseRequestTests {
                 .request(new Request.Builder().url("https://a.b.c").build())
                 .protocol(Protocol.HTTP_1_1)
                 .code(200).message("OK").body(
-                   ResponseBody.create(
-                        "{ \"id\": \"zzz\" }",
-                        MediaType.parse("application/json")
+                   ResponseBody.create(MediaType.parse("application/json"), 
+                        "{ \"id\": \"zzz\" }"
                 ))
                 .addHeader("Content-Type", "application/json")
                 .build();
