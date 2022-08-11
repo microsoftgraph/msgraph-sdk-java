5fe0b596618ed14ee368b71a6712051aaa3a12a7
ramsessanchez <ramses.sanchez99@gmail.com>
2022-08-10 18:34:13 -0700

    GraphServiceClient

--- src/main/java/com/microsoft/serviceClient/GraphServiceClient.java
+++ src/main/java/com/microsoft/serviceClient/GraphServiceClient.java
@@ -1,4 +1,17 @@
 package com.microsoft.serviceClient;
 
-public class GraphServiceClient {
+import com.microsoft.kiota.RequestAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class GraphServiceClient extends microsoft.graph.BaseGraphServiceClient {
+
+    /**
+     * Instantiates a new BaseGraphServiceClient and sets the default values.
+     *
+     * @param requestAdapter The request adapter to use to execute the requests.
+     * @return a void
+     */
+    public GraphServiceClient(@NotNull RequestAdapter requestAdapter) {
+        super(requestAdapter);
+    }
 }
