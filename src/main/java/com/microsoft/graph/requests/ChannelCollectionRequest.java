f341e926cc7234a53c78a8f3e634a39306260985
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2023-03-08 07:53:51 +0000

    Update generated files with build 108339

--- src/main/java/com/microsoft/graph/requests/ChannelCollectionRequest.java
+++ src/main/java/com/microsoft/graph/requests/ChannelCollectionRequest.java
@@ -7,7 +7,7 @@ package com.microsoft.graph.requests;
 
 import com.microsoft.graph.http.IRequestBuilder;
 import com.microsoft.graph.core.ClientException;
-import com.microsoft.graph.models.Team;
+import com.microsoft.graph.models.DeletedTeam;
 import com.microsoft.graph.models.Channel;
 import com.microsoft.graph.models.ProvisionChannelEmailResult;
 import com.microsoft.graph.models.ChatMessage;
