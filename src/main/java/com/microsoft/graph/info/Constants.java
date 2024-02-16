edd1c5a575fb634731ed35934ff2a0384d995887
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2024-02-16 10:02:19 +0000

    Update generated files with build 135643

--- src/main/java/com/microsoft/graph/info/Constants.java
+++ src/main/java/com/microsoft/graph/info/Constants.java
@@ -3,6 +3,7 @@ package com.microsoft.graph.info;
 /** Multi-purpose constants holder used accross the SDK */
 public final class Constants {
     /** The SDK version */
-    public static final String VERSION_NAME = "6.2.0";
+    public static final String VERSION_NAME = "6.3.0";
 }
+
 
