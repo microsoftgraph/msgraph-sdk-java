b55a08df79f4f72db8651621b19f8f2915e4b681
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-11 11:43:22 -0400

    spelling: across
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/main/java/com/microsoft/graph/info/Constants.java
+++ src/main/java/com/microsoft/graph/info/Constants.java
@@ -1,6 +1,6 @@
 package com.microsoft.graph.info;
 
-/** Multi-purpose constants holder used accross the SDK */
+/** Multi-purpose constants holder used across the SDK */
 public final class Constants {
     /** The SDK version */
     // x-release-please-start-version
