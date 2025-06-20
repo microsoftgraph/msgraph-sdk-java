45132613d0bb237e23d6f6981da15001b7e24d74
Vincent Biret <vibiret@microsoft.com>
2025-06-20 12:05:13 -0400

    ci: fixes zip path

--- scripts/zipPackageContent.ps1
+++ scripts/zipPackageContent.ps1
@@ -1,18 +1,17 @@
 # Checks that expected files are present & have contents after the publish process to the local cache
 param(
   [Parameter(Mandatory=$true)][string] $ArtifactId,
-  [Parameter(Mandatory=$true)][string] $PackageSegment,
   [Parameter(Mandatory=$true)][string] $Version,
   [Parameter(Mandatory=$true)][string] $OutputDirectory,
-  [Parameter()][string] $GroupId = "com.microsoft.kiota",
+  [Parameter()][string] $GroupId = "com.microsoft.graph",
   [Parameter()][string] $ComponentsSegment = "..\components",
   [Parameter()][string] $buildOutDir = "build\publishing-repository"
 )
 
-#components\bundle\build\publishing-repository\com\microsoft\kiota\microsoft-kiota-bundle\1.8.5\
+#build\publishing-repository\com\microsoft\kiota\microsoft-kiota-bundle\1.8.5\
 
 $groupIdPath = $GroupId -replace "\.", [System.IO.Path]::DirectorySeparatorChar
-$packageFullPath = Join-Path -Path $PSScriptRoot -ChildPath $ComponentsSegment -AdditionalChildPath $PackageSegment, $buildOutDir, $groupIdPath, $ArtifactId, $Version
+$packageFullPath = Join-Path -Path $PSScriptRoot -ChildPath $buildOutDir -AdditionalChildPath $groupIdPath, $ArtifactId, $Version
 
 Write-Output "---------------------------------------------------"
 Write-Output "Zipping package contents at $packageFullPath"
